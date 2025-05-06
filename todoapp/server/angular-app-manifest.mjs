c3e4c826aa1cbc332e8d15da02f95f630be195ec
Molxi <88435790+DA-Molxi@users.noreply.github.com>
2025-05-06 20:38:42 -0300

    Add files via upload

--- todoapp/server/angular-app-manifest.mjs
+++ todoapp/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 705, hash: 'b2c29114c8538bfbd7eef138a9fa8868fb3dd15732b827106fc02e5356766afa', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1004, hash: 'b2431d7ee775f3e20aa2f8bba72f768c7941c8e832b82746626da9914201c8fe', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 7180, hash: '4412f2d64656ae027a88a3bccedf8d68f58a40699cda5f846e7c8fdd48e0e7d2', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 705, hash: '279f00bda5bcf5f9a28f4792cd54b0cf5a7c9cb257848fe435316efb2f1e2956', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1004, hash: '60cb9e4dcad722304e5c5c7df6f98a1e394324c9f5b4e06d4dd41565467a7667', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 2455, hash: '516d5d9b95f76535bcf1d8af5aa7251ab1e48e098c6e77718c45589c352d5316', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-IOQZTRB7.css': {size: 1187, hash: 'ekYTHvV+q0Q', text: () => import('./assets-chunks/styles-IOQZTRB7_css.mjs').then(m => m.default)}
   },
 };
